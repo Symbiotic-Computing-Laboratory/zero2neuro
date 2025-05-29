595f781c08267f76094c15aab1a8a4b8b7a47c5e
lukeinloop <lukesewell2005@yahoo.com>
2025-05-29 15:44:22 -0500

    Modified batch,data,experiment, and network files to work with iris. Started work on the iris notebooks.

---
+++ examples/iris/batch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#SBATCH --partition=normal
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=2G
+#SBATCH --output=logs/iris_%j_stdout.txt
+#SBATCH --error=logs/iris_%j_stderr.txt
+#SBATCH --time=00:05:00
+#SBATCH --job-name=iri_test
+#SBATCH --mail-user=YOUR EMAIL ADDRESS HERE
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=YOUR DIRECTORY HERE
+
+#################################################
+# Do not change this line unless you have your own python/tensorflow/keras set up 
+
+. /home/fagg/tf_setup.sh
+conda activate dnn
+
+export ZERO2NEURO_PATH=$NEURO_REPOSITORY_PATH/zero2neuro/src
+
+
+####
+
+python $ZERO2NEURO_PATH/zero2neuro.py @network.txt @data.txt @experiment.txt -vvv --wandb --wandb_project 'iris training' --force
