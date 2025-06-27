b6ca6ccd33195833a4774d92c6d153934eee8784
lukeinloop <lukesewell2005@yahoo.com>
2025-06-27 16:00:57 -0500

    Added batch size functionality, made default 1.

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,4 @@
-'''
+    '''
 Zero2Neuro Command Line Parser
 
 Author: Andrew H. Fagg (andrewhfagg@gmail.com)
@@ -52,7 +52,7 @@ def create_parser():
     parser.add_argument('--data_output_sparse_categorical', action='store_true', help='Translate output column into sparse categorical representation')
 
     # TF Dataset configuration
-    parser.add_argument('--batch', type=int, default=None, help="Training set batch size")
+    parser.add_argument('--batch', type=int, default=1, help="Training set batch size")
     parser.add_argument('--prefetch', type=int, default=3, help="Number of batches to prefetch")
     parser.add_argument('--num_parallel_calls', type=int, default=4, help="Number of threads to use during batch construction")
     parser.add_argument('--cache', type=str, default=None, help="Cache (default: none; RAM: specify empty string; else specify file")
