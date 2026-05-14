6b26c27787d075e49e3e75023b2af082d427946a
Andrew H. Fagg <andrewhfagg@gmail.com>
2026-05-14 13:05:37 -0500

    added metrics_weighted options

--- src/parser.py
+++ src/parser.py
@@ -91,6 +91,7 @@ def create_parser(description='Zero2Neuro'):
 
     # TODO: change to list of lists
     parser.add_argument('--metrics', nargs='+', type=str, default=[], help="Metrics to compute")
+    parser.add_argument('--metrics_weighted', nargs='+', type=str, default=[], help="Metrics to compute (respecting sample weights)")
     
     parser.add_argument('--rotation', type=int, default=None, help='Expired')
     parser.add_argument('--epochs', type=int, default=100, help='Training epochs')
